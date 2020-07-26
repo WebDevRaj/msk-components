ee1d4e9988373645abb5c7a52255100010fe64e8
Rajesh Sharma <rajesh.vbn@gmail.com>
2020-07-27 00:58:02 +0530

    add description for education component

--- src/modules/resume/education/metadata.js
+++ src/modules/resume/education/metadata.js
@@ -2,7 +2,7 @@
 const metadata = {
   id: 'resume-education',
   name: "Education",
-  description: ""
+  description: "A brief about your education in reverse chronological order."
 }
 
 export default metadata;
